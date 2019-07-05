82fb50f20202ec5f24cf2b9b905e01405e69cc60
Travis Avery <travisavery82@gmail.com>
2019-07-05 13:44:59 -0500

    Minor code cleanup

--- convertable_types.py
+++ convertable_types.py
@@ -23,20 +23,24 @@ class __ImplicitlyConvertedSearchDataProvider(SearchFieldDataProvider):
         '''
 
         # Save the underlying object as a string for debugging
-        self.underlying_obj = obj
         self.__fields = []
+        self.__underlying_obj = obj
 
-        # 1) If the object is a dictionary, wrap the key/value pairs as Field objects
+        # 1) If the object is a dictionary, wrap the key/value pairs as 
+        #    SearchField objects.
         if isinstance(obj, dict):
             self.__fields = [SearchField(k,v) for k,v in obj.items()]
 
-        # 2) If the object is a list, create Field objects for each value were the key is the index
+        # 2) If the object is a list, create SearchField objects for each value
+        #    were the key is the index.
         elif isinstance(obj, list):
             self.__fields = [SearchField(k,v) for k, v in enumerate(obj)]
 
-        # 3) If the object does not implement SearchFieldDataProvider, iterate through all key/value pairs
-        #    in the objects __dict__.  For any object that is not callable and does not start with '_',
-        #    Create a SearchField object and add it to the list of fields
+        # 3) If the object does not implement SearchFieldDataProvider, 
+        #    iterate through all attributes and properties in the object.
+        #    For all properties and public (i.e. does not start with '_'),
+        #    callable, attributes, create a SearchField object and add it to 
+        #    the list of fields.
         elif not isinstance(obj, SearchFieldDataProvider):
             for k,v in obj.__class__.__dict__.items():
                 if type(v) == property:
@@ -46,13 +50,18 @@ class __ImplicitlyConvertedSearchDataProvider(SearchFieldDataProvider):
                 if not k.startswith('_') and not callable(v):
                     self.__fields.append(SearchField(k,v))
             
-        # 4) If the object implements SearchFieldDataProvider
+        # 4) If the object implements SearchFieldDataProvider, store the 
+        #    objects fields.
         else:
             self.__fields = obj.fields
 
     @property
     def fields(self):
         return self.__fields
+
+    @property
+    def underlying_object(self):
+        return self.__underlying_obj
 
     def __str__(self):
         return str(self.__underlying_obj)
@@ -68,11 +77,13 @@ def implicit_conversion(func):
     def wrapper(self, values):
         # Ensure values is a list
         if not isinstance(values, collections.Collection):
-            raise ValueError('values must be a list')
+            raise ValueError('values must be a collection')
 
-        return [r.underlying_obj for r in func(
-            self,
-            [__ImplicitlyConvertedSearchDataProvider(v) for v in values])
+        return [
+            r.underlying_object for r in func(
+                self,
+                [__ImplicitlyConvertedSearchDataProvider(v) for v in values]
+            )
         ]
 
     return wrapper
