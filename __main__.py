4b269feb96f6dc7d9e6e5269a3e58121c300b214
Travis Avery <travisavery82@gmail.com>
2019-07-24 10:49:36 -0500

    cleaning up __main__

--- __main__.py
+++ __main__.py
@@ -1,44 +1,51 @@
 # __main__.py
 
-from .unittests import run
 from .unittests import execute_query
 from .query import Query
 
 from argparse import ArgumentParser
 import logging
 import sys
+import re
 
-def is_quit(s):
-    return s.lower().strip() == 'quit' or s.lower().strip() == 'exit'
+if sys.version_info[0] >= 3:
+    raw_input = input  # use input() on Python 3
+
+
+def run_query(query_str, dryrun=False, debug=False):
+    if query_str.lower().strip() in ['quit', 'exit']:
+        return True
+
+    if dryrun:
+        print(str(Query(query_str)))
+    else:
+        execute_query(query_str, debug=debug)
 
 if __name__ == '__main__':
 
     parser = ArgumentParser()
-    parser.add_argument('cmd', nargs='*')
+    parser.add_argument('cmds', nargs='*')
     parser.add_argument('-d', '--debug', action='store_true')
+    parser.add_argument('-dr', '--dryrun', action='store_true')
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.DEBUG)# if args.debug else logging.INFO)
+    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
 
-    cmd = ' '.join(args.cmd)
+    cmds = ' '.join(args.cmds)
 
-    if sys.version_info[0] >= 3:
-        raw_input = input  # use input() on Python 3
+    if cmds:
+        for cmd in re.split(',|;', cmds):
+            if cmd and run_query(cmd):
+                exit(0)
+        else:
+            print()
 
     while True:
-        search_str = raw_input('search > ') if not cmd else cmd
+        search_str = raw_input('search > ')
 
         if not search_str:
             continue
 
-        # if search_str.lower().strip() == 'quit' \
-                # or search_str.lower().strip() == 'exit':
-        if is_quit(search_str):
+        if run_query(search_str, dryrun=args.dryrun, ):
             break
-
-        if args.debug:
-            print(str(Query(search_str)))
-        else:
-            execute_query(search_str, debug=args.debug)
         print()
-        cmd = None
