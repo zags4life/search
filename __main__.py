ed5336cdd061c7bf2ee011dd4585393b83b58b59
Travis Avery <travisavery82@gmail.com>
2019-07-30 07:43:32 -0500

    moving execute_query to __main__.py

--- __main__.py
+++ __main__.py
@@ -1,15 +1,56 @@
 # __main__.py
 
-from .unittests import execute_query
-from .query import Query
-
 from argparse import ArgumentParser
+from datetime import datetime
 import logging
 import sys
 import re
 
+
+from .query import Query
+from .unittests.testobject import TestObject
+
+
 if sys.version_info[0] >= 3:
     raw_input = input  # use input() on Python 3
+
+def execute_query(search_str, dry_run=False, debug=False):
+    values = [
+        {'x': 2, 'y': 2, 'foo': 3},
+        {'x': 2, 'yy': 20, 'foo': 3},
+
+        dict(x=1, y=2, foo='bar'),
+        dict(x='3', y=2, foo='gurp'),
+        dict(x=3, y=2, foo='gurp'),
+        [1,2,3,4],
+        {'name': 'Mike', 'fo0d': 'bar'},
+        {'name': 'Mike', 'fo0d': 'bar', 'date': datetime.today()},
+        {'name': 'mike', 'food': 'bar'},
+
+        TestObject(x=3, y=2, foo='travis'),
+        TestObject(name='Travis', age='None'),
+        TestObject(name='Tim', age='*'),
+        TestObject(name='.', age='192')
+    ]
+
+    try:
+        q = Query(search_str)
+
+        if not dry_run:
+            print('{}'.format(q))
+
+        results = q(values)
+
+        if not dry_run:
+            print('\nResults:')
+
+            for v in results:
+                print(' '*4, v)
+
+            print('{} results found - out of {} records'.format(len(results), len(values)))
+
+    except InvalidQueryError as e:
+        print('Invalid Query: {}'.format(e))
 
 
 def run_query(query_str, dryrun=False, debug=False):
--- unittests/__init__.py
+++ unittests/__init__.py
@@ -11,8 +11,11 @@ logger = logging.getLogger(__name__)
 
 REGISTERED_UNITTESTS = []
 
-def test(func):
+def unittest(func):
+    '''Decorator to register tests
+    '''
     REGISTERED_UNITTESTS.append(func)
+test = unittest
 
 def run(tests_to_run):
     result = 0
@@ -27,41 +30,3 @@ def run(tests_to_run):
         except AssertionError as e:
             logger.error(e)
     return result
-
-def execute_query(search_str, dry_run=False, debug=False):
-    values = [
-        {'x': 2, 'y': 2, 'foo': 3},
-        {'x': 2, 'yy': 20, 'foo': 3},
-
-        dict(x=1, y=2, foo='bar'),
-        dict(x='3', y=2, foo='gurp'),
-        dict(x=3, y=2, foo='gurp'),
-        [1,2,3,4],
-        {'name': 'Mike', 'fo0d': 'bar'},
-        {'name': 'Mike', 'fo0d': 'bar', 'date': datetime.today()},
-        {'name': 'mike', 'food': 'bar'},
-
-        TestObject(x=3, y=2, foo='travis'),
-        TestObject(name='Travis', age='None'),
-        TestObject(name='Tim', age='*'),
-        TestObject(name='.', age='192')
-    ]
-
-    try:
-        q = Query(search_str)
-
-        if not dry_run:
-            print('{}'.format(q))
-
-        results = q(values)
-
-        if not dry_run:
-            print('\nResults:')
-
-            for v in results:
-                print(' '*4, v)
-
-            print('{} results found - out of {} records'.format(len(results), len(values)))
-
-    except InvalidQueryError as e:
-        print('Invalid Query: {}'.format(e))
