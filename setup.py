373423e30ea8c8851bbb6b3f275b9587d8628527
Travis Avery <travisavery82@gmail.com>
2022-12-20 10:19:18 -0600

    updating setup version to 2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='search',
-    version='2.0.0',
+    version='2.1.0',
     author='Travis Avery',
     python_requires='>=3.7',
     packages=find_packages()
